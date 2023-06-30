70e15f68b639176bb5c643e0d9695e5176deccbf
22Precious <105167321+22Precious@users.noreply.github.com>
2023-06-30 22:21:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function speak()
+{
+   
+    setTimeout(function()
+    {
+    img_id ="selfie1";
+    take_snapshot();
+    speak_data="Taking your first selfie in 5 seconds";
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    SpeechSynthesis.speak(utterThis);
+    }, 5000);
+
+
+    setTimeout(function()
+    {
+    img_id ="selfie2";
+    take_snapshot();
+    speak_data="Taking your next selfie in 5 seconds";
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    SpeechSynthesis.speak(utterThis);
+    }, 10000);
+
+    setTimeout(function()
+    {
+    img_id ="selfie3";
+    take_snapshot();
+    speak_data="Taking your last selfie in 5 seconds";
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    SpeechSynthesis.speak(utterThis);
+    }, 15000);
+    
+}
+
+function take_snapshot()
+{
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri){
+    if(img_id=="selfie1"){
+        document.getElementById("result1").innerHTML='<img id="selfie1 src="'+data_uri+'"/>';
+    }
+    if(img_id=="selfie2"){
+        document.getElementById("result2").innerHTML='<img id="selfie2 src="'+data_uri+'"/>';
+    }
+    if(img_id=="selfie3"){
+        document.getElementById("result3").innerHTML='<img id="selfie3 src="'+data_uri+'"/>';
+    }
+    });
+    
+}
